6ff2290da3897cb1a6627c80fe4ff1186fda9c12
Peshka564 <matpet0412@gmail.com>
2026-01-31 17:33:30 +0200

    Docker dev setup with watch

--- api/services/api/main.go
+++ api/services/api/main.go
@@ -5,13 +5,18 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 func main() {
     r := chi.NewRouter()
     r.Use(middleware.Logger)
+    r.Use(cors.Handler(cors.Options{
+        AllowedOrigins: []string{"http://*"},
+        AllowedMethods: []string{"GET"},
+    }))
     r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+		  w.Write([]byte("Hello, Dad!"))
     })
     http.ListenAndServe(":3000", r)
 }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,58 +1,18 @@
-import { useState, useCallback } from 'react';
-import {
-  ReactFlow,
-  applyNodeChanges,
-  applyEdgeChanges,
-  addEdge,
-  type NodeChange,
-  type EdgeChange,
-  type Edge,
-  type Connection,
-} from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
-
-interface Node {
-  id: string;
-  position: { x: number; y: number };
-  data: { label: string };
-}
-
-const initialNodes: Node[] = [
-  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
-  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
-];
-const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+import { useState } from 'react';
 
 export function App() {
-  const [nodes, setNodes] = useState(initialNodes);
-  const [edges, setEdges] = useState(initialEdges);
-
-  const onNodesChange = useCallback(
-    (changes: NodeChange<Node>[]) =>
-      setNodes(nodesSnapshot => applyNodeChanges(changes, nodesSnapshot)),
-    []
-  );
-  const onEdgesChange = useCallback(
-    (changes: EdgeChange<Edge>[]) =>
-      setEdges(edgesSnapshot => applyEdgeChanges(changes, edgesSnapshot)),
-    []
-  );
-  const onConnect = useCallback(
-    (params: Connection | Edge) =>
-      setEdges(edgesSnapshot => addEdge(params, edgesSnapshot)),
-    []
-  );
+  const [val, setVal] = useState('');
+  const onClick = async () => {
+    const res = await fetch('http://localhost:3000', {
+      method: 'GET',
+    });
+    setVal(await res.text());
+  };
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      />
-    </div>
+    <>
+      <p>{val}</p>
+      <button onClick={onClick}>Save workflow</button>
+    </>
   );
 }
